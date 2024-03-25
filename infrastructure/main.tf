26ced66704cba7470514cdb59639f2b55dcc7c1b
hasansivri <140492558+hasansivri@users.noreply.github.com>
2024-03-25 22:31:13 +0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,22 +3,22 @@ provider "aws" {
 }
 
 variable "sec-gr-mutual" {
-  default = "petclinic-k8s-mutual-sec-group"
+  default = "k8s-mutual-sec-group"
 }
 
 variable "sec-gr-k8s-master" {
-  default = "petclinic-k8s-master-sec-group"
+  default = "k8s-master-sec-group"
 }
 
 variable "sec-gr-k8s-worker" {
-  default = "petclinic-k8s-worker-sec-group"
+  default = "k8s-worker-sec-group"
 }
 
 data "aws_vpc" "name" {
   default = true
 }
 
-resource "aws_security_group" "petclinic-mutual-sg" {
+resource "aws_security_group" "mutual-sg" {
   name = var.sec-gr-mutual
   vpc_id = data.aws_vpc.name.id
 
@@ -49,7 +49,7 @@ resource "aws_security_group" "petclinic-mutual-sg" {
   }
 }
 
-resource "aws_security_group" "petclinic-kube-worker-sg" {
+resource "aws_security_group" "kube-worker-sg" {
   name = var.sec-gr-k8s-worker
   vpc_id = data.aws_vpc.name.id
 
@@ -83,7 +83,7 @@ resource "aws_security_group" "petclinic-kube-worker-sg" {
   }
 }
 
-resource "aws_security_group" "petclinic-kube-master-sg" {
+resource "aws_security_group" "kube-master-sg" {
   name = var.sec-gr-k8s-master
   vpc_id = data.aws_vpc.name.id
 
@@ -128,7 +128,7 @@ resource "aws_security_group" "petclinic-kube-master-sg" {
 resource "aws_instance" "kube-master" {
     ami = "ami-07d9b9ddc6cd8dd30"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.petclinic-kube-master-sg.id, aws_security_group.petclinic-mutual-sg.id]
+    vpc_security_group_ids = [aws_security_group.kube-master-sg.id, aws_security_group.mutual-sg.id]
     key_name = "clarus"
     subnet_id = "subnet-012b73e2614cfbe2b"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
@@ -141,15 +141,15 @@ resource "aws_instance" "kube-master" {
     }
 }
 
-resource "aws_instance" "worker-1" {
+resource "aws_instance" "worker" {
     ami = "ami-07d9b9ddc6cd8dd30"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.petclinic-kube-worker-sg.id, aws_security_group.petclinic-mutual-sg.id]
+    vpc_security_group_ids = [aws_security_group.kube-worker-sg.id, aws_security_group.mutual-sg.id]
     key_name = "clarus"
     subnet_id = "subnet-012b73e2614cfbe2b"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
-        Name = "worker-1"
+        Name = "worker"
         Project = "tera-kube-ans"
         Role = "worker"
         Id = "1"
@@ -164,7 +164,7 @@ output kube-master-ip {
 }
 
 output worker-1-ip {
-  value       = aws_instance.worker-1.public_ip
+  value       = aws_instance.worker.public_ip
   sensitive   = false
-  description = "public ip of the worker-1"
+  description = "public ip of the worker"
 }
